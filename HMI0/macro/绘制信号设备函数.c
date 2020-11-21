33259166c4fbedcc1071e05eef8a1c209eca2439
wangkun <978702087@qq.com>
2020-11-21 11:13:27 +0800

    1121日，基本实现绘制，放缩，按钮自动隐藏功能，todo：编数据测试；稳定版自此基础上修改

--- HMI0/macro/绘制信号设备函数.c
+++ HMI0/macro/绘制信号设备函数.c
@@ -66,23 +66,43 @@
 
 #define PENWIDTH 2
 
-#define FANGZUO 0
-#define FANGYOU 1
+#define FANGZUO 1
+#define FANGYOU 0
 
 #define XHJLEN 12
 
+//信号设备状态
+#define ZHANYONG 1
+#define SUOBI 2
+#define KONGXIAN 3
+#define XHJGUANBI 4
+#define XHJKAIFANG 5
+#define XHJMIEDENG 6
+#define DCDINGWEI 7
+#define DCFANWEI 8
+#define DCSIKAI 9
+
+//带参数宏定义
+
+//#define DC_JT(n) dc_jt_data_0 # n
+
+
+
 //struct definition
 typedef struct{
-	int id;
-	int xinjing;
-	int xinwei;
-	int huijing;
-	int huiwei;
-	int zhijing;
-	int zhiwei;
-	int wanjing;
-	int wanwei;
-	int style;
+	short id;
+	short xinjing;
+	short xinwei;
+	short huijing;
+	short huiwei;
+	short zhijing;
+	short zhiwei;
+	short wanjing;
+	short wanwei;
+	short style;
+
+	//动态信息
+	short zhuangtai;
 }DC_struct;
 typedef struct{
 	short id;
@@ -90,13 +110,19 @@ typedef struct{
 	short jing;
 	short wei;
 	short zuo_you;
+
+	//动态信息
+	short zhuangtai;
 }XHJ_struct;
 typedef struct{
 	short id;
 	short zuojing;
 	short zuowei;
 	short youjing;
-	short youwei;	
+	short youwei;
+
+	//动态信息
+	short zhuangtai;	
 }QD_struct;
 
 //��������pen
@@ -112,7 +138,7 @@ PenParam penblue={PS_SOLID,PENWIDTH,RGB(41,82,156)};//��
 PenParam pengreen={PS_SOLID,PENWIDTH,RGB(0,255,0)};//��
 
 PenParam penwhite={PS_SOLID,PENWIDTH,RGB(255,255,255)};//��
-PenParam penblack={PS_SOLID,PENWIDTH + 4,RGB(1,1,1)};
+PenParam penblack={PS_SOLID,PENWIDTH+2,RGB(1,1,1)};
 
 						
 
@@ -139,26 +165,48 @@ int TopDetect(int x1, int y1, int x2, int y2){
 	int ret = 3;
 	int p1x,p1y,p2x,p2y;
 
-	if(x1 <= TOP_BOUND && x2 <= TOP_BOUND){
+	if(y1 <= TOP_BOUND && y2 <= TOP_BOUND){
 		ret = DOUXIAOYU;
 	}
-	else if(x1 <= TOP_BOUND && x2 > TOP_BOUND){
+	else if(y1 <= TOP_BOUND && y2 > TOP_BOUND){
 		ret = YIDAYIXIAO;
 	}
-	else if(x1 > TOP_BOUND && x2 <= TOP_BOUND){
+	else if(y1 > TOP_BOUND && y2 <= TOP_BOUND){
 		ret = YIDAYIXIAO;
 	}
 
 	return ret;
 }
 
-void ConvertPoint(int *x1, int *y1, int *x2, int *y2){
-	if((*x1) > (*x2)){
-
+void ConvertPoint(short *x1, short *y1, short *x2, short *y2){
+	int y_level = TOP_BOUND - 3;
+	if((*y1) > (*y2)){
+		//(*x2) = (*x1) - ((*y1) - y_level)*((*x1) - (*x2))/((*y1) - (*y2));
+		//(*y2) = y_level;
 	}else{
-		
+		//(*x1) = (*x2) - ((*y2) - y_level) * ((*x2) - (*x1)) / ((*y2) - (*y1));
+		//(*y1) = y_level;
 	}
 }
+
+void ShortenDC(short *x1, short *y1, short *x2, short *y2)
+{
+	short px1 = *x1, py1 = *y1, px2 = *x2, py2 = *y2;
+
+	short xmid, ymid;
+
+	if(py1 > py2)
+	{
+		ymid = py1 - 5;
+	}
+	else
+	{
+		ymid = py1 + 5;
+	}
+	xmid = (px2 - px1) / (py2 - py1) * (ymid - py1) + px1;
+
+	(*x1) = px1; (*y1) = py1; (*x2) = px2; (*y2) = py2;
+} 
 
 
 int MacroEntry()
@@ -180,26 +228,14 @@ DC_struct dcs[dc_num] = {
 	};
 	QD_struct qds[qd_num];
 
-
-if(btnVisiable==0){
-
 	unsigned short i, j, k;
 
-	short zuox,zuoy,youx,youy, xinx, xiny, huix, huiy, zhix, zhiy, wanx, wany, xhjx, xhjy, dcwanx, dcwany, state, xhj_zuoyou;
+	short zuox,zuoy,youx,youy, xinx, xiny, huix, huiy, zhix, zhiy, wanx, wany, xhjx, xhjy, dcwanx, dcwany, ifOutScreen, xhj_zuoyou;
+	short dc_zhuangtai = ZHANYONG, qd_zhuangtai = SUOBI, xhj_zhuangtai = XHJGUANBI, dc_dfw = DCFANWEI, xhjdx, xhjdy;
 
    int xinhaowidth = 10, xinhaoheight = 10;
 
 	//经纬度-》屏幕坐标
-
-	//������
-
-	
-    if(initControl == 1){
-		
-}else if(initControl == 0)
-	{
-
-	}
 
 	DrawRect(LEFT_BOUND, TOP_BOUND, RIGHT_BOUND-LEFT_BOUND, BOTTOM_BOUND-TOP_BOUND, pennull, brhback);
 
@@ -225,7 +261,7 @@ if(btnVisiable==0){
 
 		bian_y = 0;
 
-   }else{
+   	}else{
 
 		bian_y = (weiy -touy) / (toux - weix);
 
@@ -253,6 +289,9 @@ if(btnVisiable==0){
 
 //===========================================================================================================
 
+	//根据动态信息改变颜色
+	PenParam pentemp = penblue;
+	BrushParam brhtemp = brhblue;
 
 	//������
 
@@ -268,9 +307,31 @@ if(btnVisiable==0){
 		zuox *= scaleCur; zuoy *= scaleCur;
 		youx *= scaleCur; youy *= scaleCur;
 
-		state = TopDetect(zuox, zuoy, youx, youy);
-		if(state == DOUXIAOYU) continue;		
-		DrawLine(zuox, zuoy, youx, youy, penblue);
+//dong tai xin xi gai bian
+		if(qd_zhuangtai == KONGXIAN)
+		{
+			pentemp = penblue;
+		}
+		else if(qd_zhuangtai == SUOBI)
+		{
+			pentemp = penwhite;
+		}
+		else if(qd_zhuangtai == ZHANYONG)
+		{
+			pentemp = penred;
+		}
+		else
+		{
+			pentemp = penred;
+		}
+		
+
+		ifOutScreen = TopDetect(zuox, zuoy, youx, youy);
+		if(ifOutScreen == DOUXIAOYU) continue;
+		if(ifOutScreen == YIDAYIXIAO){
+			ConvertPoint(&zuox, &zuoy, &youx, &youy);
+		}		
+		DrawLine(zuox, zuoy, youx, youy, pentemp);
 
 	}
 
@@ -278,26 +339,34 @@ if(btnVisiable==0){
 
 	for(j = 0; j < dc_num*12; j = j + 12){
 
+		int index = 0;
 		if(j < 600){
 			i = j;
+			index = 1;
 			xinx = dc_jt_data_01[i]; xiny = dc_jt_data_01[i+1];
 			huix = xinx + dc_jt_data_01[i+2]; huiy = xiny + dc_jt_data_01[i+3];
-			zhix = xinx + dc_jt_data_01[i+4]; zhiy = xiny  + dc_jt_data_01[i+5];
+			zhix = xinx + dc_jt_data_01[i+4]; zhiy = xiny + dc_jt_data_01[i+5];
 			wanx = xinx + dc_jt_data_01[i+6]; wany = xiny + dc_jt_data_01[i+7];
 		}else if( j >= 600 && j < 1200){
 			i = j - 600;
+			index = 2;
 			xinx = dc_jt_data_02[i]; xiny = dc_jt_data_02[i+1];
 			huix = xinx + dc_jt_data_02[i+2]; huiy = xiny + dc_jt_data_02[i+3];
-			zhix = xinx + dc_jt_data_02[i+4]; zhiy = xiny  + dc_jt_data_02[i+5];
+			zhix = xinx + dc_jt_data_02[i+4]; zhiy = xiny + dc_jt_data_02[i+5];
 			wanx = xinx + dc_jt_data_02[i+6]; wany = xiny + dc_jt_data_02[i+7];
 		}else
 		{
 			i = j - 1200;
-			xinx = dc_jt_data_03[i]; xiny = dc_jt_data_01[i+1];
+			index = 3;
+			xinx = dc_jt_data_03[i]; xiny = dc_jt_data_03[i+1];
 			huix = xinx + dc_jt_data_03[i+2]; huiy = xiny + dc_jt_data_03[i+3];
-			zhix = xinx + dc_jt_data_03[i+4]; zhiy = xiny  + dc_jt_data_03[i+5];
+			zhix = xinx + dc_jt_data_03[i+4]; zhiy = xiny + dc_jt_data_03[i+5];
 			wanx = xinx + dc_jt_data_03[i+6]; wany = xiny + dc_jt_data_03[i+7];
 		}
+		//xinx = DC_JT(index, 0); xiny = DC_JT(index, 1);
+		//huix = xinx + DC_JT(index, 2); huiy = xiny + DC_JT(index, 3);
+		//zhix = xinx + DC_JT(index, 4); zhiy = xiny + DC_JT(index, 5);
+		//wanx = xinx + DC_JT(index, 6); wany = xiny + DC_JT(index, 7);
 		
 
 		xinx -= originx; xiny -=originy;
@@ -310,17 +379,60 @@ if(btnVisiable==0){
 		zhix *= scaleCur; zhiy *= scaleCur;
 		wanx *= scaleCur; wany *= scaleCur;
 
-		 state = TopDetect(xinx, xiny, huix, huiy);
-		if(state != DOUXIAOYU){
-			DrawLine(xinx, xiny, huix, huiy, penblue);
+//dong tai xin xi gai bian
+		if(dc_zhuangtai == KONGXIAN)
+		{
+			pentemp = penblue;
 		}
-	    state = TopDetect(xinx, xiny, zhix, zhiy);
-		if(state != DOUXIAOYU){
-			DrawLine(xinx, xiny, zhix, zhiy, penblue);
+		else if(dc_zhuangtai == SUOBI)
+		{
+			pentemp = penwhite;
 		}
-		state = TopDetect(xinx, xiny, wanx, wany);
-		if(state != DOUXIAOYU){
-			DrawLine(xinx, xiny, wanx, wany, penblue);
+		else if(dc_zhuangtai == ZHANYONG)
+		{
+			pentemp = penred;
+		}
+		else
+		{
+			pentemp = penred;
+		}
+
+		ifOutScreen = TopDetect(xinx, xiny, huix, huiy);
+		if(ifOutScreen != DOUXIAOYU){
+			DrawLine(xinx, xiny,  huix, huiy, pentemp);
+		}
+	    ifOutScreen = TopDetect(xinx, xiny, zhix,  zhiy);
+		if(ifOutScreen != DOUXIAOYU){
+			DrawLine(xinx, xiny,  zhix,  zhiy, pentemp);
+		}
+		ifOutScreen = TopDetect(xinx, xiny,  wanx, wany);
+		if(ifOutScreen != DOUXIAOYU){
+			DrawLine(xinx, xiny, wanx,  wany, pentemp);
+		}
+
+		if(dc_dfw == DCDINGWEI)
+		{
+			short dx = xinx + (wanx - xinx) / 4;
+			short dy = xiny + (wany - xiny) / 4;
+			ifOutScreen = TopDetect(xinx, xiny,  dx, dy);
+			if(ifOutScreen != DOUXIAOYU){				
+				DrawLine(xinx, xiny, dx, dy, penblack);
+				DrawLine(xinx, xiny, zhix, zhiy, pentemp);
+			}
+		}
+		else if(dc_dfw == DCFANWEI)
+		{
+			short dx = xinx + (zhix - xinx) / 4;
+			short dy = xiny + (zhiy - xiny) / 4;
+			ifOutScreen = TopDetect(xinx, xiny,  dx, dy);
+			if(ifOutScreen != DOUXIAOYU){			
+				DrawLine(xinx, xiny, dx, dy, penblack);
+				DrawLine(xinx, xiny, wanx, wany, pentemp);
+			}
+		}
+		else
+		{
+		
 		}
 
 	}
@@ -330,11 +442,6 @@ if(btnVisiable==0){
 	for(j = 0; j < xhj_num * 9; j = j + 9){
 
 		//������
-
-		//��Բ
-		/*   xhjx = xhjs[i].jing; xhjy = xhjs[i].wei;
-		xhjx -= originx; xhjy -= originy; 
-		xhjx *= scale; xhjy *= scale;   */
 
 		if(j < 900){
 			i = j;
@@ -352,15 +459,34 @@ if(btnVisiable==0){
 
 		xhjx *= scaleCur; xhjy *= scaleCur;
 
+//dong tai xin xi gai bian
+		if(xhj_zhuangtai == XHJKAIFANG)
+		{
+			pentemp = penwhite;
+			brhtemp = brhwhite;
+		}
+		else if(xhj_zhuangtai == XHJGUANBI)
+		{
+			pentemp = penblue;
+			brhtemp = brhblue;
+		}
+		else
+		{
+			pentemp = penblue;
+			brhtemp = brhblue;
+		}
+		
+		xinhaowidth = 15 * scaleCur, xinhaoheight = 15 * scaleCur;
+		xhjdx = 15 * scaleCur; xhjdy = 10 * scaleCur;	
 		if(xhjx > LEFT_BOUND && xhjy > TOP_BOUND && xhjx < RIGHT_BOUND && xhjy < BOTTOM_BOUND)
 		{
-			xinhaowidth = 20, xinhaoheight = 20;			
-			if(xhj_zuoyou == FANGYOU){
-				DrawLine(xhjx, xhjy, xhjx, xhjy+10, penblue);
-				DrawEclips(xhjx-20, xhjy, xinhaowidth, xinhaoheight, penred, brhred);
-			}else if(xhj_zuoyou == FANGZUO){
-				DrawLine(xhjx, xhjy, xhjx, xhjy-15, penblue);
-				DrawEclips(xhjx, xhjy-12, xinhaowidth, xinhaoheight, penred, brhred);
+					
+			if(xhj_zuoyou == FANGZUO){
+				DrawLine(xhjx, xhjy, xhjx, xhjy-xhjdy*2, pentemp);
+				DrawEclips(xhjx-xhjdx, xhjy-xhjdy, xinhaowidth, xinhaoheight, pentemp, brhtemp);
+			}else if(xhj_zuoyou == FANGYOU){
+				DrawLine(xhjx, xhjy, xhjx, xhjy+xhjdy*2, pentemp);
+				DrawEclips(xhjx, xhjy-xhjdy, xinhaowidth, xinhaoheight, pentemp, brhtemp);
 			}
 		}
 	}
@@ -368,7 +494,6 @@ if(btnVisiable==0){
 	nowDrawStr2 = 0;
 
 	//�������豸
-}
 	return 0;
 
 } 
